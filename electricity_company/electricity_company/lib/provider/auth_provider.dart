63e66facedf996fe2aacd4c904f2b5ff34f1bc59
Reem Jarrar <reemj2003@hotmail.com>
2023-08-15 23:47:29 +0300

    otp check

--- electricity_company/electricity_company/lib/provider/auth_provider.dart
+++ electricity_company/electricity_company/lib/provider/auth_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:electricity_company/screens/authenticate/otp.dart';
 import 'package:electricity_company/utils/utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -7,9 +8,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AuthProvider extends ChangeNotifier {
   bool _isSignedIn = false;
   bool get isSignedIn => _isSignedIn;
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  String? _uid;
+  String get uid => _uid!;
 
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   AuthProvider() {
     checkSignIn();
   }
@@ -20,6 +25,7 @@ class AuthProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  //signin
   void signInWithhPhone(BuildContext context, String phoneNumber) async {
     try {
       await _firebaseAuth.verifyPhoneNumber(
@@ -42,6 +48,46 @@ class AuthProvider extends ChangeNotifier {
           codeAutoRetrievalTimeout: (verificationId) {});
     } on FirebaseAuthException catch (e) {
       showSnackBar(context, e.message.toString());
+    }
+  }
+
+  //verify otp
+  void verifyOtp({
+    required BuildContext context,
+    required String verificationId,
+    required String userOtp,
+    required Function onSuccess,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      PhoneAuthCredential creds = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: userOtp);
+      User? user = (await _firebaseAuth.signInWithCredential(creds)).user!;
+      if (user != null) {
+        _uid = user.uid;
+        onSuccess();
+      }
+      _isLoading = false;
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message.toString());
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  // database operations
+  Future<bool> checkExistingUser() async {
+    DocumentSnapshot snapshot =
+        await _firebaseFirestore.collection("users").doc(_uid).get();
+    if (snapshot.exists) {
+      print("User Exists");
+      return true;
+    } else {
+      print("New User");
+      return false;
     }
   }
 }
--- electricity_company/electricity_company/lib/screens/authenticate/otp.dart
+++ electricity_company/electricity_company/lib/screens/authenticate/otp.dart
@@ -1,4 +1,11 @@
+import 'package:electricity_company/constants/colors.dart';
+import 'package:electricity_company/provider/auth_provider.dart';
+import 'package:electricity_company/screens/authenticate/user_info.dart';
+import 'package:electricity_company/utils/utils.dart';
+import 'package:electricity_company/widgets/custome_button.dart';
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+import 'package:provider/provider.dart';
 
 class Otp extends StatefulWidget {
   final String vertificationId;
@@ -9,8 +16,138 @@ class Otp extends StatefulWidget {
 }
 
 class _OtpState extends State<Otp> {
+  String? otpCode;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final isLoading =
+        Provider.of<AuthProvider>(context, listen: true).isLoading;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBlue,
+        elevation: 0.0,
+        title: Text("شركة الكهرباء الأردنية"),
+        centerTitle: true,
+        leading: Image.asset("images/logo.png", width: 40),
+        leadingWidth: 100,
+      ),
+      body: SafeArea(
+        child: isLoading == true
+            ? Center(
+                child: CircularProgressIndicator(
+                  color: tdBlue,
+                ),
+              )
+            : Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: GestureDetector(
+                          onTap: () => Navigator.of(context).pop(),
+                          child: const Icon(Icons.arrow_back),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      const Text(
+                        'أدخل رمز التحقق',
+                        style: TextStyle(
+                            fontSize: 50,
+                            color: tdBlue,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        "تم ارسال رمز الى هاتفك للتحقق من رقم الهاتف",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Pinput(
+                        length: 6,
+                        showCursor: true,
+                        defaultPinTheme: PinTheme(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: tdBlue),
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        onCompleted: (value) {
+                          setState(() {
+                            otpCode = value;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 25),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: CustomeButton(
+                          text: "تحقق",
+                          onPressed: () {
+                            if (otpCode != null) {
+                              verifyOtp(context, otpCode!);
+                            } else {
+                              showSnackBar(
+                                  context, "أدخل الرمز المكون من ستة ارقام");
+                            }
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "لم يصلك رمز التحقق؟",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 15),
+                      const Text(
+                        "ارسل رمز اخر",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: tdBlue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+
+  //verify otp
+  void verifyOtp(BuildContext context, String userOtp) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+    ap.verifyOtp(
+      context: context,
+      verificationId: widget.vertificationId,
+      userOtp: userOtp,
+      onSuccess: () {
+        //checking whether user exists in the db
+        ap.checkExistingUser().then(
+          (value) async {
+            if (value == true) {
+              //user exists in our app
+            } else {
+              //new user
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const UserInfo()),
+                  (route) => false);
+            }
+          },
+        );
+      },
+    );
   }
 }
